d072c23e262e02c85334f66197db285c5cef85a3
Raffael Fradio <fradior.xd@gmail.com>
2021-11-18 13:42:13 +0700

    Initial Commit

---
+++ MobileProg-iOS/MobileProg_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileProg_iOSApp.swift
+//  MobileProg-iOS
+//
+//  Created by Raff on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MobileProg_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
